62d2fffbeed42e8cae42bfbbc0f5e2f2c10027f1
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-15 13:09:35 +0000

    fixes to constructors

--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateCensusCodeDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateCensusCodeDao.java
@@ -7,4 +7,8 @@ import org.jason.mapmaker2.model.CensusCode;
  * @author Jason Ferguson
  */
 public class HibernateCensusCodeDao extends HibernateGenericDao<CensusCode> implements CensusCodeDao {
+
+    public HibernateCensusCodeDao() {
+        super(CensusCode.class);
+    }
 }
--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLocationDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLocationDao.java
@@ -7,4 +7,8 @@ import org.jason.mapmaker2.model.Location;
  * @author Jason Ferguson
  */
 public class HibernateLocationDao extends HibernateGenericDao<Location> implements LocationDao {
+
+    public HibernateLocationDao() {
+        super(Location.class);
+    }
 }
