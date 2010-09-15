9a44a1aaac265c85f419e66a0abe1595582df753
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-15 01:57:47 +0000

    initial commit

---
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateCensusCodeDao.java
@@ -0,0 +1,10 @@
+package org.jason.mapmaker2.dao.hibernate;
+
+import org.jason.mapmaker2.dao.CensusCodeDao;
+import org.jason.mapmaker2.model.CensusCode;
+
+/**
+ * @author Jason Ferguson
+ */
+public class HibernateCensusCodeDao extends HibernateGenericDao<CensusCode> implements CensusCodeDao {
+}
