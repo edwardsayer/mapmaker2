524c5e6c964bd78f9ab0c4b1b9714674767bc34e
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-16 14:56:37 +0000

    added @Repository

--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLocationDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLocationDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.LocationDao;
 import org.jason.mapmaker2.model.Location;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
+@Repository("locationDao")
 public class HibernateLocationDao extends HibernateGenericDao<Location> implements LocationDao {
 
     public HibernateLocationDao() {
