82b2bcc98c512f9463bd37d8e17fca4bc02a75b6
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-16 14:56:37 +0000

    added @Repository

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateCensusCodeDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateCensusCodeDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.CensusCodeDao;
 import org.jason.mapmaker2.model.CensusCode;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
+@Repository("censusCodeDao")
 public class HibernateCensusCodeDao extends HibernateGenericDao<CensusCode> implements CensusCodeDao {
 
     public HibernateCensusCodeDao() {
--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLineDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateLineDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.LineDao;
 import org.jason.mapmaker2.model.Line;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
+@Repository("lineDao")
 public class HibernateLineDao extends HibernateGenericDao<Line> implements LineDao {
 
         public HibernateLineDao() {
