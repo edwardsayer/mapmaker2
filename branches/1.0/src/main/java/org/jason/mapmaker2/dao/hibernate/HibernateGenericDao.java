7cfb668663df4422d64afd702257bc17dd931fe8
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-16 15:17:42 +0000

    added init() for hibernatetemplate to stop Hibernate from whining

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateGenericDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateGenericDao.java
@@ -1,11 +1,17 @@
 package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.GenericDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import java.util.List;
 
 /**
+ * GenericDAO for all other Hibernate DAOs to extend.
+ * <p/>
+ * Classes wishing to make use of QBE should minimize use of primitives over Object wrappers.
+ *
  * @author Jason Ferguson
  */
 public class HibernateGenericDao<T> extends HibernateDaoSupport implements GenericDao<T> {
@@ -14,6 +20,11 @@ public class HibernateGenericDao<T> extends HibernateDaoSupport implements Gener
 
     public HibernateGenericDao(Class<T> type) {
         this.type = type;
+    }
+
+    @Autowired
+    public void init(HibernateTemplate hibernateTemplate) {
+        setHibernateTemplate(hibernateTemplate);
     }
 
     public T save(T obj) {
