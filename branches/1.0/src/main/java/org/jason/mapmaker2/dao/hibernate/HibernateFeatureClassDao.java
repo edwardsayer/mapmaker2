f7cae2b3b408b92bf3bf394a612dd43c63620c0c
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-17 14:52:27 +0000

    forgot the @Repository annotation

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateFeatureClassDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateFeatureClassDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.FeatureClassDao;
 import org.jason.mapmaker2.model.FeatureClass;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author TSgt Jason Ferguson
  */
+@Repository("featureClassDao")
 public class HibernateFeatureClassDao extends HibernateGenericDao<FeatureClass> implements FeatureClassDao {
 
     public HibernateFeatureClassDao() {
