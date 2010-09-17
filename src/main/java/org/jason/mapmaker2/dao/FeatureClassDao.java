40296a77b4acff5407b05d4523f9bb916f5a9b02
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-17 14:51:50 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/dao/FeatureClassDao.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.dao;
+
+import org.jason.mapmaker2.model.FeatureClass;
+
+/**
+ * @author TSgt Jason Ferguson
+ */
+public interface FeatureClassDao extends GenericDao<FeatureClass> {
+}
