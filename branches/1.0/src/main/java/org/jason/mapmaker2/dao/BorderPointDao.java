b8f1f8fe2fba801b22d72044305775921683b13a
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-17 19:03:24 +0000

    initial commit

---
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/BorderPointDao.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.dao;
+
+import org.jason.mapmaker2.model.BorderPoint;
+
+/**
+ * @author Jason Ferguson
+ */
+public interface BorderPointDao extends GenericDao<BorderPoint> {
+}
