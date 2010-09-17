fe6f9f766a0da0bceae61cc93e34b0f97527697a
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-17 15:09:41 +0000

    added validation, input result

--- src/main/java/org/jason/mapmaker2/web/action/FeatureClassAction.java
+++ src/main/java/org/jason/mapmaker2/web/action/FeatureClassAction.java
@@ -1,10 +1,12 @@
 package org.jason.mapmaker2.web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.interceptor.validation.SkipValidation;
 import org.jason.mapmaker2.model.FeatureClass;
 import org.jason.mapmaker2.service.FeatureClassService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,8 @@ import java.util.List;
  */
 @Namespace("/featureClass")
 @Results({
-        @Result(name="success", location = "/WEB-INF/content/admin/featureClass/list.jsp")
+        @Result(name="success", location = "/WEB-INF/content/admin/featureClass/list.jsp"),
+        @Result(name="input", location = "/WEB-INF/content/admin/featureClass/create.jsp")
 })
 public class FeatureClassAction extends ActionSupport {
 
@@ -31,6 +34,7 @@ public class FeatureClassAction extends ActionSupport {
     private String classCodeShortDesc;
     private String classCodeDesc;
 
+    @RequiredStringValidator(message="You must provide the feature class code!")
     public String getClassCode() {
         return classCode;
     }
@@ -39,6 +43,7 @@ public class FeatureClassAction extends ActionSupport {
         this.classCode = classCode;
     }
 
+    @RequiredStringValidator(message="You must provide a short description for the description")
     public String getClassCodeShortDesc() {
         return classCodeShortDesc;
     }
@@ -66,6 +71,7 @@ public class FeatureClassAction extends ActionSupport {
     }
 
     @Action("")
+    @SkipValidation
     public String execute() throws Exception {
 
         featureClasses = featureClassService.getAll();
