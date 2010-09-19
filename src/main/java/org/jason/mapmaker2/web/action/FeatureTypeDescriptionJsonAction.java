006389b9a4944b5cf79f1d0be27097457f47851b
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-19 18:59:12 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/web/action/FeatureTypeDescriptionJsonAction.java
@@ -0,0 +1,52 @@
+package org.jason.mapmaker2.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.jason.mapmaker2.model.FeatureTypeDescription;
+import org.jason.mapmaker2.service.FeatureTypeDescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Jason Ferguson
+ */
+@ParentPackage("json-default")
+public class FeatureTypeDescriptionJsonAction extends ActionSupport {
+
+    private FeatureTypeDescriptionService featureTypeDescriptionService;
+
+    @Autowired
+    public void setFeatureTypeDescriptionService(FeatureTypeDescriptionService featureTypeDescriptionService) {
+        this.featureTypeDescriptionService = featureTypeDescriptionService;
+    }
+
+    private List<FeatureTypeDescription> featureTypeDescriptions;
+
+    public List<FeatureTypeDescription> getFeatureTypeDescriptions() {
+        return featureTypeDescriptions;
+    }
+
+    public void setFeatureTypeDescriptions(List<FeatureTypeDescription> featureTypeDescriptions) {
+        this.featureTypeDescriptions = featureTypeDescriptions;
+    }
+
+    public String execute() throws Exception {
+
+        return SUCCESS;
+    }
+
+    @Action(value="featureTypeDescriptsionJson", results = {
+            @Result(name="success", type = "json")
+    })
+    public String getFeatureTypeDescriptionsJson() throws Exception {
+
+        featureTypeDescriptions = featureTypeDescriptionService.getAll();
+        Collections.sort(featureTypeDescriptions);
+
+        return SUCCESS;
+    }
+}
