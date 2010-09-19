f9cac1f2d4a4c18587deabd08293862cd32e6a90
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-19 18:45:38 +0000

    overrode defaults

--- branches/1.0/src/main/java/org/jason/mapmaker2/service/ServiceException.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/service/ServiceException.java
@@ -5,5 +5,19 @@ package org.jason.mapmaker2.service;
  */
 public class ServiceException extends Exception {
 
+    public ServiceException() {
+        super();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 
+    public ServiceException(String message) {
+        super(message);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
