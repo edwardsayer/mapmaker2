6f009c25d343f0b4c03fe55fb47328dd494c1ea8
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 00:48:36 +0000

    initial commit

---
+++ branches/1.0/src/main/java/org/jason/mapmaker2/service/BorderPointService.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.service;
+
+import org.jason.mapmaker2.model.BorderPoint;
+
+/**
+ * @author Jason Ferguson
+ */
+public interface BorderPointService extends BasicServiceOperations<BorderPoint>{
+}
