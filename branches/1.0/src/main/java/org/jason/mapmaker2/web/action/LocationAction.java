28744ea45917fa7dd142a0faa0d3ee4994bca5e1
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 20:25:45 +0000

    changed value for the @Action annotation.

--- branches/1.0/src/main/java/org/jason/mapmaker2/web/action/LocationAction.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/web/action/LocationAction.java
@@ -24,7 +24,7 @@ public class LocationAction extends ActionSupport {
         this.locationService = locationService;
     }
 
-    @Action("/")
+    @Action("")
     @SkipValidation
     public String execute() throws Exception {
 
