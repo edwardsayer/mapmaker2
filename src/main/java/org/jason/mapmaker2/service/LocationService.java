4ac9864a6d265968f2f74bb9d72e4cec4966e15e
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 00:42:57 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/service/LocationService.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.service;
+
+import org.jason.mapmaker2.model.Location;
+
+/**
+ * @author Jason Ferguson
+ */
+public interface LocationService extends BasicServiceOperations<Location> {
+}
