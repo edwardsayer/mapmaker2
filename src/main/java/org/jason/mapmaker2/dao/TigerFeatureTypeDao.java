7d6e70c8d7f7667f48cd7660a0205da143a93a99
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 15:29:25 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/dao/TigerFeatureTypeDao.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.dao;
+
+import org.jason.mapmaker2.model.TigerFeatureType;
+
+/**
+ * @author TSgt Jason Ferguson
+ */
+public interface TigerFeatureTypeDao extends GenericDao<TigerFeatureType>{
+}
