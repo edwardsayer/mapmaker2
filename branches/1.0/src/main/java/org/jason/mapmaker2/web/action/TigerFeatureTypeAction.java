1866bfd55e35f4c319738a6649b1bb09660e2d0e
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 16:40:20 +0000

    filled in create() method

--- branches/1.0/src/main/java/org/jason/mapmaker2/web/action/TigerFeatureTypeAction.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/web/action/TigerFeatureTypeAction.java
@@ -2,6 +2,7 @@ package org.jason.mapmaker2.web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.convention.annotation.*;
+import org.jason.mapmaker2.model.TigerFeatureType;
 import org.jason.mapmaker2.service.TigerFeatureTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -29,6 +30,25 @@ public class TigerFeatureTypeAction extends ActionSupport {
         return SUCCESS;
     }
 
+    private String tigerFeatureClassCode;
+    private String tigerFeatureClassCodeDescription;
+
+    public String getTigerFeatureClassCode() {
+        return tigerFeatureClassCode;
+    }
+
+    public void setTigerFeatureClassCode(String tigerFeatureClassCode) {
+        this.tigerFeatureClassCode = tigerFeatureClassCode;
+    }
+
+    public String getTigerFeatureClassCodeDescription() {
+        return tigerFeatureClassCodeDescription;
+    }
+
+    public void setTigerFeatureClassCodeDescription(String tigerFeatureClassCodeDescription) {
+        this.tigerFeatureClassCodeDescription = tigerFeatureClassCodeDescription;
+    }
+
     @Action("showCreate")
     public String showCreate() throws Exception {
 
@@ -38,6 +58,17 @@ public class TigerFeatureTypeAction extends ActionSupport {
     @Action("create")
     public String create() throws Exception {
 
+        TigerFeatureType tft = new TigerFeatureType();
+        tft.setTigerFeatureClassCode(tigerFeatureClassCode);
+        tft.setDescription(tigerFeatureClassCodeDescription);
+
+        TigerFeatureType result = tigerFeatureTypeService.save(tft);
+        if (result == null) {
+            addActionError("Unable to save new TIGER Feature Class Code");
+            return INPUT;
+        }
+
+        addActionMessage("Successfully created new TIGER Feature Class Code");
         return SUCCESS;
     }
 }
