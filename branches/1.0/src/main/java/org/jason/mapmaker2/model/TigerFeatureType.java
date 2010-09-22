39339f4b7ea2b8e3f03505d489468d1b587cf2fa
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-22 15:28:48 +0000

    rewritten as class since hibernate makes enums painful

--- branches/1.0/src/main/java/org/jason/mapmaker2/model/TigerFeatureType.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/model/TigerFeatureType.java
@@ -1,19 +1,62 @@
 package org.jason.mapmaker2.model;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * @author Jason Ferguson
  */
-public enum TigerFeatureType {
+@Entity
+@Table(name = "T_TIGERFEATURETYPE")
+public class TigerFeatureType implements Serializable {
 
-    COUNTY(1),
-    CONGRESSIONALDISTRICT(2);
+    private Integer id;
+    private String tigerFeatureClassCode;
+    private String description;
 
-    private TigerFeatureType(int t) {
-        type = 1;
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
     }
-    private int type;
 
-    public int getType() {
-        return type;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name="CLASSCODE")
+    public String getTigerFeatureClassCode() {
+        return tigerFeatureClassCode;
+    }
+
+    public void setTigerFeatureClassCode(String tigerFeatureClassCode) {
+        this.tigerFeatureClassCode = tigerFeatureClassCode;
+    }
+
+    @Column(name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TigerFeatureType)) return false;
+
+        TigerFeatureType that = (TigerFeatureType) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
