bc1f6059ed964ad6c7d3e795a65be04ad5da3e59
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-23 11:13:18 +0000

    more work

--- src/main/java/org/jason/mapmaker2/web/action/StateCodeAction.java
+++ src/main/java/org/jason/mapmaker2/web/action/StateCodeAction.java
@@ -2,8 +2,11 @@ package org.jason.mapmaker2.web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.convention.annotation.*;
+import org.jason.mapmaker2.model.StateCode;
 import org.jason.mapmaker2.service.StateCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * @author Jason Ferguson
@@ -23,18 +26,59 @@ public class StateCodeAction extends ActionSupport {
         this.stateCodeService = stateCodeService;
     }
 
+    private String stateName;
+    private String stateAbbr;
+    private String stateCode;
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getStateAbbr() {
+        return stateAbbr;
+    }
+
+    public void setStateAbbr(String stateAbbr) {
+        this.stateAbbr = stateAbbr;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    List<StateCode> stateCodeList;
+
     @Action("")
     public String execute() throws Exception {
+
+        stateCodeList = stateCodeService.getAll();
         return SUCCESS;
     }
 
     @Action("showCreate")
     public String showCreate() throws Exception {
+
         return INPUT;
     }
 
     @Action("create")
     public String create() throws Exception {
+
+        StateCode result = stateCodeService.save(new StateCode(stateName, stateAbbr, stateCode));
+
+        if (result==null) {
+            addActionError("Unable to save State Code!");
+            return INPUT;
+        }
+        
         return SUCCESS;
     }
 }
