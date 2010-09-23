0c0f417863edb84b13a2a756869cc69a8658281f
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-23 19:58:34 +0000

    added @Repository

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateSubCodeDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateSubCodeDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.SubCodeDao;
 import org.jason.mapmaker2.model.SubCode;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
+@Repository("subCodeDao")
 public class HibernateSubCodeDao extends HibernateGenericDao<SubCode> implements SubCodeDao {
 
     public HibernateSubCodeDao() {
