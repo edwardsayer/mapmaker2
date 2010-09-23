7040b03eb8c8eb705b1e21744416639a4c65bf00
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-23 11:05:44 +0000

    removed @Respository annotation

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/StateCodeDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/StateCodeDao.java
@@ -1,11 +1,9 @@
 package org.jason.mapmaker2.dao;
 
 import org.jason.mapmaker2.model.StateCode;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
-@Repository("stateCode")
 public interface StateCodeDao extends GenericDao<StateCode> {
 }
