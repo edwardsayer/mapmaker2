1f859ff6c1d9fb79f50ff4845e61d36a965ca48b
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-23 11:04:42 +0000

    initial commit

--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateStateCodeDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateStateCodeDao.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.dao.hibernate;
 
 import org.jason.mapmaker2.dao.StateCodeDao;
 import org.jason.mapmaker2.model.StateCode;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Jason Ferguson
  */
+@Repository("stateCodeDao")
 public class HibernateStateCodeDao extends HibernateGenericDao<StateCode> implements StateCodeDao {
 
     public HibernateStateCodeDao() {
