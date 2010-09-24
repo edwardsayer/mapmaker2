0c41d385bac58c8f32192355e4fc9fffea12f9ca
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-24 20:45:35 +0000

    saveBpSet() is now just saveAll()

--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateBorderPointDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateBorderPointDao.java
@@ -1,8 +1,11 @@
 package org.jason.mapmaker2.dao.hibernate;
 
+import org.hibernate.Transaction;
 import org.jason.mapmaker2.dao.BorderPointDao;
 import org.jason.mapmaker2.model.BorderPoint;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 /**
  * @author Jason Ferguson
@@ -13,4 +16,28 @@ public class HibernateBorderPointDao extends HibernateGenericDao<BorderPoint> im
     public HibernateBorderPointDao() {
         super(BorderPoint.class);
     }
+
+    @Override
+    public void saveAll(Collection<BorderPoint> objList) {
+
+        Transaction tx = getSession().beginTransaction();
+
+        int counter = 0;
+        for (BorderPoint bp : objList) {
+            getSession().save(bp);
+            counter++;
+            if (counter == 200) {
+                System.out.println("Commiting 50 BorderPoints");
+                if (getSession().isDirty()) {
+                    getSession().flush();
+                    getSession().clear();
+                }
+                counter = 0;
+                //tx.commit();
+            }
+        }
+
+        tx.commit();
+    }
+
 }
