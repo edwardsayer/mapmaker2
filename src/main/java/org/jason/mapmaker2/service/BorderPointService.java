c7e1b3acbf1dc87de6eaadba6926270ca530b17b
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-24 20:46:00 +0000

    saveBpSet() is now just saveAll()

--- src/main/java/org/jason/mapmaker2/service/BorderPointService.java
+++ src/main/java/org/jason/mapmaker2/service/BorderPointService.java
@@ -2,8 +2,12 @@ package org.jason.mapmaker2.service;
 
 import org.jason.mapmaker2.model.BorderPoint;
 
+import java.util.Collection;
+
 /**
  * @author Jason Ferguson
  */
 public interface BorderPointService extends BasicServiceOperations<BorderPoint>{
+
+    void saveAll(Collection<BorderPoint> bpCollection);
 }
