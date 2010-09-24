2a24d7595c2a3f7883f656dc1c6ee235dd6ac067
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-24 21:27:28 +0000

    implemented logging, fixed the counter

--- src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateBorderPointDao.java
+++ src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateBorderPointDao.java
@@ -1,5 +1,7 @@
 package org.jason.mapmaker2.dao.hibernate;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Transaction;
 import org.jason.mapmaker2.dao.BorderPointDao;
 import org.jason.mapmaker2.model.BorderPoint;
@@ -13,6 +15,10 @@ import java.util.Collection;
 @Repository("borderPointDao")
 public class HibernateBorderPointDao extends HibernateGenericDao<BorderPoint> implements BorderPointDao {
 
+    private static final Log log = LogFactory.getLog(HibernateBorderPointDao.class);
+    
+    private static final int batchSize = 100;
+
     public HibernateBorderPointDao() {
         super(BorderPoint.class);
     }
@@ -20,24 +26,23 @@ public class HibernateBorderPointDao extends HibernateGenericDao<BorderPoint> im
     @Override
     public void saveAll(Collection<BorderPoint> objList) {
 
+        log.debug("Saving " + objList.size() + " BorderPoints");
         Transaction tx = getSession().beginTransaction();
 
-        int counter = 0;
+        int counter = 1;
         for (BorderPoint bp : objList) {
             getSession().save(bp);
             counter++;
-            if (counter == 200) {
-                System.out.println("Commiting 50 BorderPoints");
-                if (getSession().isDirty()) {
-                    getSession().flush();
-                    getSession().clear();
-                }
-                counter = 0;
-                //tx.commit();
+            if (counter == batchSize) {
+                log.info("Commiting " + batchSize + " BorderPoints");
+                getSession().flush();
+                getSession().clear();
+                counter = 1;
             }
         }
 
         tx.commit();
+
     }
 
 }
