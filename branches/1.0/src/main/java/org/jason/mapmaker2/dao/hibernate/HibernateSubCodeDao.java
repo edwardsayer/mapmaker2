20db2bd93f362a61e80a743e15d46a50b4b6f98c
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-27 10:57:29 +0000

    added getUniqueSubCodeTypesByStateCode

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateSubCodeDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/hibernate/HibernateSubCodeDao.java
@@ -1,8 +1,16 @@
 package org.jason.mapmaker2.dao.hibernate;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.jason.mapmaker2.dao.SubCodeDao;
+import org.jason.mapmaker2.model.StateCode;
 import org.jason.mapmaker2.model.SubCode;
+import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author Jason Ferguson
@@ -12,5 +20,21 @@ public class HibernateSubCodeDao extends HibernateGenericDao<SubCode> implements
 
     public HibernateSubCodeDao() {
         super(SubCode.class);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public List<String> getUniqueSubCodeTypesByStateCode(StateCode stateCode) {
+
+        return (List<String>) getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) throws HibernateException, SQLException {
+
+                String hql = "select distinct sc.subCodeType from SubCode sc";
+                Query query = session.createQuery(hql);
+                return query.list();
+                //return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+        //return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
