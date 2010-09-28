d2b998ece85d1a50cf3d399d5d261667640ffe66
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-28 16:39:11 +0000

    deleting TigerFeatureType since I don't use it

--- branches/1.0/src/main/java/org/jason/mapmaker2/service/TigerFeatureTypeService.java
+++
@@ -1,9 +0,0 @@
-package org.jason.mapmaker2.service;
-
-import org.jason.mapmaker2.model.TigerFeatureType;
-
-/**
- * @author TSgt Jason Ferguson
- */
-public interface TigerFeatureTypeService extends BasicServiceOperations<TigerFeatureType>{
-}
