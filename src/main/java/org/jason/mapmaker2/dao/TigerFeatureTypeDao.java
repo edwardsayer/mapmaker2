da13a1dc3a036c8e94f30e732de77d8872cf6ca3
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-28 16:39:11 +0000

    deleting TigerFeatureType since I don't use it

--- src/main/java/org/jason/mapmaker2/dao/TigerFeatureTypeDao.java
+++
@@ -1,9 +0,0 @@
-package org.jason.mapmaker2.dao;
-
-import org.jason.mapmaker2.model.TigerFeatureType;
-
-/**
- * @author TSgt Jason Ferguson
- */
-public interface TigerFeatureTypeDao extends GenericDao<TigerFeatureType>{
-}
