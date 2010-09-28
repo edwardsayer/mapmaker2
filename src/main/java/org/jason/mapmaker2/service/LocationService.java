02d9d72e6709b6addd3fb3be7595974e7ae56898
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-28 16:40:38 +0000

    deleting Location since I don't use it

--- src/main/java/org/jason/mapmaker2/service/LocationService.java
+++
@@ -1,9 +0,0 @@
-package org.jason.mapmaker2.service;
-
-import org.jason.mapmaker2.model.Location;
-
-/**
- * @author Jason Ferguson
- */
-public interface LocationService extends BasicServiceOperations<Location> {
-}
