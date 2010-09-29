a5cc11c703a5494e479e5ec8eafb1262484df030
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-29 13:28:35 +0000

    added createMap()

--- branches/1.0/src/main/java/org/jason/mapmaker2/service/CustomMapService.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/service/CustomMapService.java
@@ -1,7 +1,11 @@
 package org.jason.mapmaker2.service;
 
+import org.jason.mapmaker2.model.composite.CustomMap;
+
 /**
  * @author Jason Ferguson
  */
 public interface CustomMapService {
+
+    CustomMap createMap(Integer stateId, Integer subCodeId) throws ServiceException;
 }
