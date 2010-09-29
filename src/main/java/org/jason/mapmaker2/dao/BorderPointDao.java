2c27fcb43a44944266a8ae5fe228892304ba20af
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-29 17:08:48 +0000

    added getMinimumLatitude

--- src/main/java/org/jason/mapmaker2/dao/BorderPointDao.java
+++ src/main/java/org/jason/mapmaker2/dao/BorderPointDao.java
@@ -1,10 +1,13 @@
 package org.jason.mapmaker2.dao;
 
 import org.jason.mapmaker2.model.BorderPoint;
+import org.jason.mapmaker2.model.StateCode;
+import org.jason.mapmaker2.model.SubCode;
 
 /**
  * @author Jason Ferguson
  */
 public interface BorderPointDao extends GenericDao<BorderPoint> {
 
+    Float getMinimumLatitude(StateCode stateCode, SubCode subCode);
 }
