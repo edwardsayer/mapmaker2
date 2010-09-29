7c0d775aa8a7c8238c49c36db3159a477fc3b842
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-09-29 13:28:13 +0000

    added getByStateCodeAndSubCode()

--- src/main/java/org/jason/mapmaker2/service/BorderPointService.java
+++ src/main/java/org/jason/mapmaker2/service/BorderPointService.java
@@ -1,8 +1,11 @@
 package org.jason.mapmaker2.service;
 
 import org.jason.mapmaker2.model.BorderPoint;
+import org.jason.mapmaker2.model.StateCode;
+import org.jason.mapmaker2.model.SubCode;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Jason Ferguson
@@ -10,4 +13,6 @@ import java.util.Collection;
 public interface BorderPointService extends BasicServiceOperations<BorderPoint>{
 
     void saveAll(Collection<BorderPoint> bpCollection);
+
+    List<BorderPoint> getByStateCodeAndSubCode(StateCode stateCode, SubCode subCode);
 }
