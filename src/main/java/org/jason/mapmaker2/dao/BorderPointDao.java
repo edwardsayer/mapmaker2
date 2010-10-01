d0c5397bd749e70e5cb20d8b018767f6142839a3
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-01 17:57:30 +0000

    added getByStateCodeAndSubCode(final StateCode stateCode, final SubCode subCode), QBE wasn't working

--- src/main/java/org/jason/mapmaker2/dao/BorderPointDao.java
+++ src/main/java/org/jason/mapmaker2/dao/BorderPointDao.java
@@ -4,10 +4,14 @@ import org.jason.mapmaker2.model.BorderPoint;
 import org.jason.mapmaker2.model.StateCode;
 import org.jason.mapmaker2.model.SubCode;
 
+import java.util.List;
+
 /**
  * @author Jason Ferguson
  */
 public interface BorderPointDao extends GenericDao<BorderPoint> {
+
+    List<BorderPoint> getByStateCodeAndSubCode(StateCode stateCode, SubCode subCode);
 
     Float getMinimumLatitude(StateCode stateCode, SubCode subCode);
 }
