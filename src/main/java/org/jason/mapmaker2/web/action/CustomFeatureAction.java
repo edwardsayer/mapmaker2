3641411dd5ef5b31eb635e6346e39c1c32e5e9be
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-04 16:47:39 +0000

    added struts annotations

--- src/main/java/org/jason/mapmaker2/web/action/CustomFeatureAction.java
+++ src/main/java/org/jason/mapmaker2/web/action/CustomFeatureAction.java
@@ -1,12 +1,20 @@
 package org.jason.mapmaker2.web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.*;
+import org.apache.struts2.interceptor.validation.SkipValidation;
 import org.jason.mapmaker2.service.CustomFeatureService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Jason Ferguson
  */
+@ParentPackage("json-default")
+@Namespace("/customFeature")
+@Results({
+        @Result(name = "success", location = "/WEB-INF/content/admin/customFeature/list.jsp"),
+        @Result(name = "input", location = "/WEB-INF/content/admin/customFeature/create.jsp")
+})
 public class CustomFeatureAction extends ActionSupport {
 
     private CustomFeatureService customFeatureService;
@@ -16,7 +24,20 @@ public class CustomFeatureAction extends ActionSupport {
         this.customFeatureService = customFeatureService;
     }
 
+    @Action("")
+    @SkipValidation
     public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+    @Action("showCreate")
+    public String showCreate() throws Exception {
+        return INPUT;
+    }
+
+    @Action("create")
+    public String create() throws Exception {
+
         return SUCCESS;
     }
 }
