6a52be6aa2f5aeccd88440ea9ab7a0cea50822c7
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-04 16:34:45 +0000

    fixed stupid mistake

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/CustomFeatureDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/CustomFeatureDao.java
@@ -1,7 +1,9 @@
 package org.jason.mapmaker2.dao;
 
+import org.jason.mapmaker2.model.CustomFeature;
+
 /**
  * @author Jason Ferguson
  */
-public interface CustomFeatureDao extends GenericDao<CustomFeatureDao>{
+public interface CustomFeatureDao extends GenericDao<CustomFeature>{
 }
