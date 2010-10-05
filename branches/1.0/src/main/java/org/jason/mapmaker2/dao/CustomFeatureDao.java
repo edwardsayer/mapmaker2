1cb86f10f1657b3e5482c36bc995da2dbad8839b
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-05 11:39:09 +0000

    added getCustomFeaturesWithBounds

--- branches/1.0/src/main/java/org/jason/mapmaker2/dao/CustomFeatureDao.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/dao/CustomFeatureDao.java
@@ -2,8 +2,12 @@ package org.jason.mapmaker2.dao;
 
 import org.jason.mapmaker2.model.CustomFeature;
 
+import java.util.List;
+
 /**
  * @author Jason Ferguson
  */
 public interface CustomFeatureDao extends GenericDao<CustomFeature>{
+
+    List<CustomFeature> getCustomFeaturesWithBounds(float minLat, float maxLat, float minLng, float maxLng);
 }
