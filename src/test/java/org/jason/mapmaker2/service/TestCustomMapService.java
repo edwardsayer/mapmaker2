c5131ad5fff17d44bf3e54ca6e124969bac1d281
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-05 23:00:18 +0000

    initial commit

---
+++ src/test/java/org/jason/mapmaker2/service/TestCustomMapService.java
@@ -0,0 +1,34 @@
+package org.jason.mapmaker2.service;
+
+import junit.framework.TestCase;
+import org.jason.mapmaker2.model.composite.CustomMap;
+import org.junit.Test;
+
+/**
+ * @author Jason Ferguson
+ */
+public class TestCustomMapService extends TestCase {
+
+    private CustomMapService customMapService = new CustomMapServiceImpl();
+
+    @Test
+    public void createMapShouldThrowAnExceptionWithAnInvalidStateCode() {
+
+        try {
+            CustomMap customMap = customMapService.createMap(null, null);
+            fail("Invalid state code should throw a Service Exception");
+        } catch (ServiceException e) {
+        }
+
+    }
+
+    @Test
+    public void createMapShouldThrowAnExceptionWithAnInvalidSubCode() {
+
+        try {
+            CustomMap customMap = customMapService.createMap(null, null);
+            fail("Invalid subcode should throw a ServiceException");
+        } catch (ServiceException e) {
+        }
+    }
+}
