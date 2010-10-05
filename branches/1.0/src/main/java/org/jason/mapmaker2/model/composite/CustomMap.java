db76a0c6733799d2ed7e924f8cd12a79723fca67
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-05 11:39:55 +0000

    added list of features

--- branches/1.0/src/main/java/org/jason/mapmaker2/model/composite/CustomMap.java
+++ branches/1.0/src/main/java/org/jason/mapmaker2/model/composite/CustomMap.java
@@ -1,6 +1,7 @@
 package org.jason.mapmaker2.model.composite;
 
 import org.jason.mapmaker2.model.BorderPoint;
+import org.jason.mapmaker2.model.CustomFeature;
 import org.jason.mapmaker2.model.StateCode;
 import org.jason.mapmaker2.model.SubCode;
 
@@ -19,6 +20,7 @@ public class CustomMap implements Serializable {
     private List<BorderPoint> borderPoints;
     private StateCode stateCode;
     private SubCode subCode;
+    private List<CustomFeature> customFeatures;
 
     public Float getMinLat() {
         return minLat;
@@ -74,5 +76,14 @@ public class CustomMap implements Serializable {
 
     public void setSubCode(SubCode subCode) {
         this.subCode = subCode;
+    }
+
+    public List<CustomFeature> getCustomFeatures() {
+        
+        return customFeatures;
+    }
+
+    public void setCustomFeatures(List<CustomFeature> customFeatures) {
+        this.customFeatures = customFeatures;
     }
 }
