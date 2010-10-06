ae1678575339593b90aefc9b9f16afd6fb5af230
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-06 11:56:47 +0000

    added getCustomFeatureTypes()

--- src/main/java/org/jason/mapmaker2/service/CustomMapService.java
+++ src/main/java/org/jason/mapmaker2/service/CustomMapService.java
@@ -2,10 +2,12 @@ package org.jason.mapmaker2.service;
 
 import org.jason.mapmaker2.model.composite.CustomMap;
 
+import java.util.List;
+
 /**
  * @author Jason Ferguson
  */
 public interface CustomMapService {
 
-    CustomMap createMap(Integer stateId, Integer subCodeId) throws ServiceException;
+    CustomMap createMap(Integer stateId, Integer subCodeId, List<String> featureTypes) throws ServiceException;
 }
