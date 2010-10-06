3d999dd53d847ed1649ca4babb4a141458194ae7
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-06 23:16:43 +0000

    remove dead code

--- src/main/java/org/jason/mapmaker2/web/action/DefaultAction.java
+++ src/main/java/org/jason/mapmaker2/web/action/DefaultAction.java
@@ -20,12 +20,6 @@ import java.util.List;
 @Result(name = "success", location = "/WEB-INF/content/makeMap.jsp")
 public class DefaultAction extends ActionSupport {
 
-//    Map<String,String[]> parameters;
-//
-//    public void setParameters(Map<String, String[]> parameters) {
-//        this.parameters = parameters;
-//    }
-
     private StateCodeService stateCodeService;
 
     @Autowired
@@ -33,34 +27,7 @@ public class DefaultAction extends ActionSupport {
         this.stateCodeService = stateCodeService;
     }
 
-//    private SubCodeService subCodeService;
-//
-//    @Autowired
-//    public void setSubCodeService(SubCodeService subCodeService) {
-//        this.subCodeService = subCodeService;
-//    }
-//
-//    private int stateCodeId;
-//    private int subCodeId;
-//
-//    public int getStateCodeId() {
-//        return stateCodeId;
-//    }
-//
-//    public void setStateCodeId(int stateCodeId) {
-//        this.stateCodeId = stateCodeId;
-//    }
-//
-//    public int getSubCodeId() {
-//        return subCodeId;
-//    }
-//
-//    public void setSubCodeId(int subCodeId) {
-//        this.subCodeId = subCodeId;
-//    }
-
     List<StateCode> stateCodeList;
-    //List<SubCode> subCodeList;
 
     public List<StateCode> getStateCodeList() {
         return stateCodeList;
@@ -69,14 +36,6 @@ public class DefaultAction extends ActionSupport {
     public void setStateCodeList(List<StateCode> stateCodeList) {
         this.stateCodeList = stateCodeList;
     }
-//
-//    public List<SubCode> getSubCodeList() {
-//        return subCodeList;
-//    }
-//
-//    public void setSubCodeList(List<SubCode> subCodeList) {
-//        this.subCodeList = subCodeList;
-//    }
 
     @Action("")
     public String execute() throws Exception {
@@ -95,92 +54,4 @@ public class DefaultAction extends ActionSupport {
         return SUCCESS;
     }
 
-//    @Action(value = "getStateCodesJson", results = {
-//            @Result(name = "success", type = "json")
-//    })
-//    public String getStateCodesJson() throws Exception {
-//        stateCodeList = stateCodeService.getAll();
-//        Collections.sort(stateCodeList);
-//
-//        return SUCCESS;
-//    }
-//
-//    private List<String> distinctSubCodes;
-//
-//    public List<String> getDistinctSubCodes() {
-//        return distinctSubCodes;
-//    }
-//
-//    public void setDistinctSubCodes(List<String> distinctSubCodes) {
-//        this.distinctSubCodes = distinctSubCodes;
-//    }
-//
-//    @Action(value = "getSubCodesJson", results = {
-//            @Result(name = "success", type = "json")
-//    })
-//    public String getSubCodesJson() throws Exception {
-//
-//        if (parameters != null && parameters.get("id") != null) {
-//            stateCodeId = Integer.parseInt(parameters.get("id")[0]);
-//            StateCode stateCode = stateCodeService.getById(stateCodeId);
-//            distinctSubCodes = subCodeService.getUniqueSubCodeTypesByStateCode(stateCode);
-//        }
-//
-//        return SUCCESS;
-//    }
-//
-//    private List<String> descriptions;
-//
-//    public List<String> getDescriptions() {
-//        return descriptions;
-//    }
-//
-//    public void setDescriptions(List<String> descriptions) {
-//        this.descriptions = descriptions;
-//    }
-//
-//    private List<SubCode> subCodes;
-//
-//    public List<SubCode> getSubCodes() {
-//        return subCodes;
-//    }
-//
-//    public void setSubCodes(List<SubCode> subCodes) {
-//        this.subCodes = subCodes;
-//    }
-//
-//    @Action(value="getSubCodeDescriptionsByFeatureTypeJson", results = {
-//            @Result(name="success", type = "json"),
-//            @Result(name="input", location = "/WEB-INF/content/makeMap.jsp")
-//    })
-//    public String getSubCodeDescriptionsByFeatureType() throws Exception {
-//
-//        // TODO: need to get all of the subcodes back by feature type, not a list of Strings
-//        // get the subcodes by stateId and feature type (i.e. county names if the feature is a county)
-//        if (parameters != null) {
-//            if (parameters.get("stateCodeId") == null) {
-//                addActionError("No State Code ID received!");
-//                return SUCCESS;
-//            }
-//
-//            if (parameters.get("featureName") == null) {
-//                addActionError("No Feature Name received for AJAX call!");
-//                return SUCCESS;
-//            }
-//
-//            StateCode stateCode = stateCodeService.getById(stateCodeId);
-//            String featureType = parameters.get("featureName")[0];
-//            subCodes = subCodeService.getByStateCodeAndFeatureType(stateCode, featureType);
-//            Collections.sort(subCodes, new Comparator<SubCode>() {
-//                public int compare(SubCode o1, SubCode o2) {
-//                    return o1.getSubCodeDescription().compareTo(o2.getSubCodeDescription());
-//                }
-//            });
-//
-//            return SUCCESS;
-//
-//        }
-//
-//        return SUCCESS;
-//    }
 }
