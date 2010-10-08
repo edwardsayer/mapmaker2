bf91043398910bd966bfc1130cb76dfcee1de3b8
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-08 12:42:11 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/model/util/Geonames.java
@@ -0,0 +1,20 @@
+package org.jason.mapmaker2.model.util;
+
+/**
+ * Geonames.java
+ *
+ * Utility class for a USGS GEONAMES Feature file.
+ *
+ * I hate enums.
+ *
+ * @author Jason Ferguson
+ */
+public class Geonames {
+
+    public static final int FEATUREID = 0;
+    public static final int DESCRIPTION = 1;
+    public static final int FEATURECLASS = 2;
+    public static final int STATEFIPS = 4;
+    public static final int LATITUDE = 9;
+    public static final int LONGITUDE = 10;
+}
