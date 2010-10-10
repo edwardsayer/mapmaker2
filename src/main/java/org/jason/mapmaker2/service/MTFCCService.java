e6c61ad7fd5629bb15c9e92002b618ef2eb22756
fergusonjason <fergusonjason@46dc516b-01cf-225e-7655-4900ade0bc17>
2010-10-10 19:23:45 +0000

    initial commit

---
+++ src/main/java/org/jason/mapmaker2/service/MTFCCService.java
@@ -0,0 +1,9 @@
+package org.jason.mapmaker2.service;
+
+import org.jason.mapmaker2.model.MTFCC;
+
+/**
+ * @author Jason Ferguson
+ */
+public interface MTFCCService extends BasicServiceOperations<MTFCC> {
+}
